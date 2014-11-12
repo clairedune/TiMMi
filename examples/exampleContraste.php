e259212c3e86be43baf9c49ea7ef2ee53992a136
Claire Dune <claire.dune@gmail.com>
2014-11-12 19:29:34 +0100

    ajout de la classe contraste qui contient toutes les modif de contrastes

---
+++ examples/exampleContraste.php
@@ -0,0 +1,42 @@
+<?php
+	/*! 
+	 * \file exampleIntro.php
+	 * \brief ce fichier montre un premier exemple de chargement d'une image et d'affichage
+	 * \author Claire Dune
+	 * \date 20/10/2013
+	 */
+
+	require_once("../conf/config.php");
+	require_once("../src/Lecteur.php");
+    require_once("../src/colorMode.php");
+    require_once("../src/Contraste.php");
+       
+       
+	$lecteur = new Lecteur();
+	$lecteur->ouvre("../images/bobine.png");
+    $lecteur->afficheImage();
+    
+    $Isrc = $lecteur->exporte();
+    $I = clone $Isrc;
+    $IbNw = ColorMode::desaturation( $I );
+   
+    // importer l'image resultat
+    $lecteur->importe($IbNw ,"../res/bobineLumDesat.png");
+    $lecteur->afficheImage();
+        
+    $Iclair = Contraste::decalage($IbNw,50);
+    $lecteur->importe($Iclair ,"../res/bobclaire.png");
+    $lecteur->afficheImage();
+   
+   
+    $Isombre = Contraste::decalage($IbNw,-50);
+    $lecteur->importe($Isombre ,"../res/bobsombre.png");
+    $lecteur->afficheImage();
+    
+    $Iauto = Contraste::automatique($IbNw);
+    $lecteur->importe($Iauto ,"../res/bobAuto.png");
+    $lecteur->afficheImage();
+    
+   
+?>
+
---
+++ src/Contraste.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once('Image.php');
+
+class Contraste
+{
+    
+    public static function decalage($Isrc,$dec)
+    {
+        // pour tous les pixels on ajoute $decalage à la valeur
+        $Ires = clone $Isrc;
+        for( $i = 0 ; $i < $Isrc->getLargeur() ; $i++)
+            for( $j = 0 ; $j < $Isrc->getLargeur() ; $j++) 
+            {
+                // on ajoute la valeur
+                $val = $Isrc->tab[$i][$j][0] + $dec; 
+                
+                // on sature le resultat pour qu'il soit affichable
+                if($val>255) $val = 255;
+                else if ($val < 0) $val = 0;
+                
+                // on construit la nouvelle image
+                $Ires->tab[$i][$j][0] = $val;
+                $Ires->tab[$i][$j][1] = $val;
+                $Ires->tab[$i][$j][2] = $val;
+            }
+        
+        // on renvoie le resultat
+        return $Ires;
+    }
+    
+    public static function automatique($Isrc)
+    {
+        
+        $mini = $Isrc->minimum();
+        $maxi = $Isrc->maximum();
+        $plage = $maxi-$mini;
+        
+       
+        
+        
+       
+         if ($plage==0)
+         {
+            echo "la plage de valeur est nulle, l'image ne peut pas être améliorée";
+            return $Isrc;
+         }
+         
+         $Ires = clone $Isrc;
+         
+        for( $i = 0 ; $i < $Isrc->getLargeur() ; $i++)
+            for( $j = 0 ; $j < $Isrc->getLargeur() ; $j++) 
+            {
+                // on ajoute la valeur
+                $val = ($Isrc->tab[$i][$j][0]-$mini)*255/$plage; 
+                
+                // on sature le resultat pour qu'il soit affichable
+                if($val>255) $val = 255;
+                else if ($val < 0) $val = 0;
+                
+                // on construit la nouvelle image
+                $Ires->tab[$i][$j][0] = $val;
+                $Ires->tab[$i][$j][1] = $val;
+                $Ires->tab[$i][$j][2] = $val;
+            }
+        
+        // on renvoie le resultat
+        return $Ires;
+        
+    }
+    
+    
+    
+}
